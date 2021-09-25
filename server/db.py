0a46cf5dfefc4a130fb9e2c0af2aeed4e4ed982b
ricky42613 <ricky42613@gmail.com>
2021-09-25 17:28:47 +0800

    server for operate mongoDB

--- server/db.py
+++ server/db.py
@@ -2,16 +2,16 @@ from pymongo import MongoClient
 import hashlib
 
 def get_database():
+    # 對mongoDB連線
     client = MongoClient("mongodb+srv://ricky:eric033014@user.f8q8g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    # 進入igassist這個schema
     db = client['igassist']
     return db
-    
-# This is added so that many files can reuse the function get_database()
+
 if __name__ == "__main__":    
-    # Get the database
     db = get_database()
     db['user'].insert_one({
         'acn':'abcd1234',
         'passwd': hashlib.md5('abcd1234'.encode('utf-8')).hexdigest(),
-        'username': 'xiao ming'
+        'username': 'xiao hua'
     })
--- server/server.py
+++ server/server.py
@@ -4,8 +4,11 @@ import uvicorn
 import  db
 import hashlib
 app = FastAPI()
+
+# 跟db連線
 igDB = db.get_database()
 
+# 發送請求時一定要帶這些參數，不能多也不能少
 class User(BaseModel):
     acn: str
     passwd: str
@@ -18,13 +21,17 @@ def read_root():
     return {"Hello": "World"}
 
 
+#當使用者對http://127.0.0.1:8088/users/發起post請求時，透過下面function處理
 @app.post("/users/")
+# User是定義對/users/這個路徑發送post請求時需要帶什麼參數，有哪些參數要帶請看12行。
 async def save_user(user: User):
     try:
+        # 將資料整理成dict
         data = {}
         data['acn'] = user.acn
         data['passwd'] = hashlib.md5(user.passwd.encode('utf-8')).hexdigest()
         data['username'] = user.username
+        # 將整理成dict的資料存進user這張table
         igDB['user'].insert_one(data)
         return {'status':'ok'}
     except:
@@ -32,6 +39,7 @@ async def save_user(user: User):
 
 if __name__ == '__main__':
     try:
+        # 開一個伺服器在127.0.0.1:8088,用瀏覽器在網址欄打上http://127.0.0.1:8088可以檢查是否成功啟動
         uvicorn.run(app, host="127.0.0.1", port=8088)
     except:
         print('error')
